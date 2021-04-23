7c5cc86a8963f4cf096b15898cff958c48a3aced
Robert Hieger <rhieger@gmail.com>
2021-04-23 12:06:12 -0400

    Begin complete ground-up refactor of app.js

---
+++ finished-project/js/temp.js
@@ -0,0 +1,75 @@
+// Capture table cells and buttons
+const button1 = document.getElementById('addBreed');
+const button2 = document.getElementById('reset');
+const placeHolderRow = document.getElementById('noBreed');
+
+class dogBreed {
+  constructor(breed, origin, lifeSpan) {
+    this.breed = breed;
+    this.origin = origin;
+    this.lifeSpan = lifeSpan;
+  }
+}
+
+const toggleDisplay = (id) => {
+  console.log(placeHolderRow.classList);
+  placeHolderRow.classList.toggle(id);
+}
+
+/*
+
+const breed1 = {
+  breed: 'PUG',
+  origin: 'CHINA',
+  lifeSpan: '12-15 YEARS'
+};
+
+const breed2 = {
+  breed: 'LABRADOR',
+  origin: 'NEWFOUNDLAND',
+  lifeSpan: '10-12 YEARS'
+};
+
+const breed3 = {
+  breed: 'BEAGLE',
+  origin: 'ENGLAND',
+  lifeSpan: '12-15 YEARS'
+};
+
+*/
+
+const getDogInfo = {
+  info: function() {
+    // Array containing properties of each dog object
+    return [
+      this.breed,
+      this.origin,
+      this.lifeSpan];
+  }
+};
+
+const removePlaceHolder = () => {
+  placeHolderRow.classList.replace('visible', 'invisible');
+};
+
+const resetTable = () => {
+  placeHolderRow.classList.replace('invisible', 'visible');
+};
+
+const setRowContent = (
+  column1,
+  column2,
+  column3,
+  breed
+) => {
+  column1.textContent = breed[0];
+  column2.textContent = breed[1];
+  column3.textContent = breed[2];
+};
+
+// Button box event listeners populate
+// table with dog breed information.
+
+// TBD—Event Listeners
+
+button2.addEventListener('click', resetTable);

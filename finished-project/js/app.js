e888f4b6d637752ecfdc78580ede6523d8b6b505
Robert Hieger <rhieger@gmail.com>
2021-04-24 19:23:48 -0400

    Create event listener to hide table and button bar and display modal

--- finished-project/js/app.js
+++ finished-project/js/app.js
@@ -1,3 +1,17 @@
+const dogTable = document.getElementById('dogBreeds');
 const placeHolder = document.getElementById('noBreed');
 const buttonRow = document.getElementById('buttons');
 const modalDialogue = document.getElementById('modal');
+
+const openModalButton = document.getElementById('addBreed');
+
+const showModal = () => {
+  // Remove button bar
+  buttonRow.classList.replace('visible', 'invisible');
+  // Hide table
+  dogTable.classList.replace('visible', 'invisible');
+  // Show Modal Dialogue
+  modalDialogue.classList.replace('invisible', 'visible');
+};
+
+openModalButton.addEventListener('click', showModal);

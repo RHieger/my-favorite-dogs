afc764b7f5867d1eefa311805ad99ce8c10b2a9b
Robert Hieger <rhieger@gmail.com>
2021-05-08 14:35:08 -0400

    Refactor starter files for tutorial

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,12 +1,11 @@
-// Capture table cells and buttons
+// Declare DOM Objects for table, modal and buttons
 
 /* YOUR CODE HERE */
 
-/* 
- * Button box event listeners populate table with
- * dog breed information
- * 
-*/
+// Call back funmctions
 
 /* YOUR CODE HERE */
 
+// Event Listeners
+
+/* YOUR CODE HERE */

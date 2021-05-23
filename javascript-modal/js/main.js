0566a311d8a04112e0fda3e9129266e0b14f1d91
Robert Hieger <rhieger@gmail.com>
2021-05-22 23:05:32 -0400

    Create openModal() function

--- javascript-modal/js/main.js
+++ javascript-modal/js/main.js
@@ -1,3 +1,8 @@
 let imageSF = document.querySelector('.trip-image');
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
+
+const openModal = () => {
+ backdrop.style.dispay = 'block';
+ modal.style.dispay = 'block';
+};

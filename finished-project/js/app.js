764d6f761d4eb80b7a9b6cd77da09eefcb68cb41
Robert Hieger <rhieger@gmail.com>
2021-07-25 18:08:18 -0400

    Rebuild DOM model for app and dog breed objects

---
+++ finished-project/js/app.js
@@ -0,0 +1,36 @@
+// Capture table cells and buttons
+const breedOne = document.getElementById('favBreed1');
+const breedOrigin1 = document.getElementById('origin1');
+const breedAvgLife1 = document.getElementById('avgLife1');
+
+const breedTwo = document.getElementById('favBreed2');
+const breedOrigin2 = document.getElementById('origi2');
+const breedAvgLife2 = document.getElementById('avgLife2');
+
+const breedThree = document.getElementById('favBreed3');
+const breedOrigin3 = document.getElementById('origin3');
+const breedAvgLife3 = document.getElementById('avgLife3');
+
+const button1 = document.getElementById('breed1');
+const button2 = document.getElementById('breed2');
+const button3 = document.getElementById('breed3');
+const button4 = document.getElementById('reset');
+
+// Dog objects:
+const breed1 = {
+  breed: 'pug',
+  origin: 'china',
+  lifeSpan: '12-15 years'
+};
+
+const breed2 = {
+  breed: 'labrador',
+  origin: 'newfoundland',
+  lifeSpan: '10-12 years'
+};
+
+const breed3 = {
+  breed: 'beagle',
+  origin: 'england',
+  lifeSpan: '12-15 years'
+};

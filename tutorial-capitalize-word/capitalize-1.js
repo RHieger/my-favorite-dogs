11a08bb94f18c28ed08eb9ea9067a4667795fe32
Robert Hieger <rhieger@gmail.com>
2021-10-12 23:25:28 -0400

    Add Demo for toLowerCase() Method

---
+++ tutorial-capitalize-word/capitalize-1.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Write a function that capitalizes a string.
+ * 
+ * This tutorial is based on the one found at the
+ * URL below:
+ * 
+ * https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
+ * 
+ */
+
+const name = 'sAm';
+const lower = name.toLowerCase();
+
+console.log(`\nname = ${name}\n`);
+console.log(`APPLY toLowerCase()...\n`);
+
+console.log(`OUTPUT FROM lower = name.toLowerCase()...`);
+console.log(`${lower}\n`);

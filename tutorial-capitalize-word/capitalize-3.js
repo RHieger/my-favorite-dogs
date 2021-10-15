7eef479845f6930e0beff057c50803dcb26521ea
Robert Hieger <rhieger@gmail.com>
2021-10-15 17:35:26 -0400

    Add demo of toUpperCase()method

---
+++ tutorial-capitalize-word/capitalize-3.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Write a function that capitalizes a string.
+ * 
+ * This tutorial is based on the one found at the
+ * URL below:
+ * 
+ * https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
+ * 
+ */
+
+const name = 'sAm';
+const first = name.charAt(0);
+
+const upper = first.toUpperCase();
+
+console.log(`\nname = ${name}\n`);
+console.log(`Capturing first character of name...\n`);
+console.log(`First character of ${name} = ${first}\n`);
+
+console.log('Transforming first character to uppercase...\n');
+console.log(`Uppercase \'s\' = ${upper}\n`);

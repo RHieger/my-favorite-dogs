4e898f3755eeada715888ec2cdd97967f1ef1d40
Robert Hieger <rhieger@gmail.com>
2021-10-15 19:41:30 -0400

    Add demo of slice() method

---
+++ tutorial-capitalize-word/capitalize-4.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Write a function that capitalizes a string.
+ * 
+ * This tutorial is based on the one found at the
+ * URL below:
+ * 
+ * https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
+ * 
+ */
+
+const name = 'sAm';
+const wordRemainder = name.slice(1);
+
+console.log(`\nconst name = ${name}\n`);
+
+console.log(`Remainder of word following \'s\': ${wordRemainder}\n`);

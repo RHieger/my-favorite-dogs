4626b424ca855f8312b1cfb706ce53075290d3b5
Robert Hieger <rhieger@gmail.com>
2021-10-15 21:27:54 -0400

    Add Method 3 to capitalize-me.js

--- tutorial-capitalize-word/capitalize-me.js
+++ tutorial-capitalize-word/capitalize-me.js
@@ -22,12 +22,26 @@ const capitalize2 = (word) => {
   word.slice(1).toLowerCase();
 };
 
+const capitalize3 = (word) => {
+  return word[0].toUpperCase() +
+  word.substring(1).toLowerCase();
+};
+
 console.log(`\nCapitalize Method #1:`);
-console.log(capitalize.toString(), '\n');
+console.log(`--------------------`);
+console.log('capitalize = ' + capitalize.toString(), '\n');
 console.log(`${member} capitalized is ${capitalize(member)}.\n`);
 
 // METHOD #2 Using Bracket Notation:
 
 console.log(`\nCapitalize Method #2:`);
-console.log(capitalize2.toString(), '\n');
+console.log(`--------------------`);
+console.log('capitalize2 = ' + capitalize2.toString(), '\n');
+console.log(`${member} capitalized is ${capitalize2(member)}.\n`);
+
+// METHOD #3 substring():
+
+console.log(`\nCapitalize Method #3:`);
+console.log(`--------------------`);
+console.log('capitalize3 = ' + capitalize3.toString(), '\n');
 console.log(`${member} capitalized is ${capitalize2(member)}.\n`);
